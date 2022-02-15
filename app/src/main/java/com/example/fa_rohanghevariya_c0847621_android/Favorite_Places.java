e330933d152b4422cc208017ac7148dbef9206b4
Rohan <roh987gh@yahoo.com>
2022-02-15 09:07:13 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/fa_rohanghevariya_c0847621_android/Favorite_Places.java
@@ -0,0 +1,14 @@
+package com.example.fa_rohanghevariya_c0847621_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Favorite_Places extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_places);
+    }
+}
---
+++ app/src/main/java/com/example/fa_rohanghevariya_c0847621_android/MapsActivity.java
@@ -0,0 +1,246 @@
+package com.example.fa_rohanghevariya_c0847621_android;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.fa_rohanghevariya_c0847621_android.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    Location currentLocation;
+    public static final String FINE_LOCATION= Manifest.permission.ACCESS_FINE_LOCATION;
+    public static final String COARSE_LOCATION=Manifest.permission.ACCESS_COARSE_LOCATION;
+    public static final int REQUEST_CODE=1011;
+    private Boolean mLocationPermisssionGranted=false;
+    FusedLocationProviderClient mfusedLocationProviderClient;
+    public static final float Default_zoom=15f;
+    private Object LatLng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        getPermission();
+        init();
+    }
+    //method to search any address
+    private void init(){
+        binding.searchLocation.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId== EditorInfo.IME_ACTION_SEARCH
+                ||actionId==EditorInfo.IME_ACTION_DONE){
+                    geoLocate();
+                }
+
+                return false;
+            }
+        });
+        binding.icGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MapsActivity.this, "Your Current Location", Toast.LENGTH_SHORT).show();
+                getDeviceLocation();
+            }
+        });
+        binding.placePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickCurrentPlace(address);
+            }
+        });
+
+        HideSoftKeyboard();
+    }
+
+    private void geoLocate(){
+        String searchString=binding.searchLocation.getText().toString();
+        Geocoder geocoder= new Geocoder(MapsActivity.this);
+        List<Address> list=new ArrayList<>();
+        try {
+            list=geocoder.getFromLocationName(searchString,1);
+        }catch (IOException e){
+
+        }
+        if(list.size()>0){
+            Address address=list.get(0);
+
+            moveFocus(new LatLng(address.getLatitude(),address.getLongitude()),Default_zoom,address.getAddressLine(0));
+        }
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng currentLatLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(currentLatLng).title("Marker at Your Current Location"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+        if(mLocationPermisssionGranted){
+            getDeviceLocation();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            init();
+        }
+    }
+// for favorite place
+    private void pickCurrentPlace( LatLng latLng) {
+        if (mMap == null) {
+            return;
+        }
+
+        if (mLocationPermisssionGranted ) {
+            getDeviceLocation();
+        } else {
+            // The user has not granted permission.
+            Log.i(TAG, "The user did not grant location permission.");
+
+            // Add a default marker, because the user hasn't selected a place.
+            mMap.addMarker(new MarkerOptions()
+                    .title("Default Location")
+                    .position(latLng)
+                    .snippet("No places found, because location permission is disabled"));
+
+            // Prompt the user for permission.
+            getPermission();
+        }
+    }
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (mLocationPermisssionGranted) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                Task<Location> locationResult = mfusedLocationProviderClient.getLastLocation();
+                locationResult.addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            // Set the map's camera position to the current location of the device.
+                            Location mLastKnownLocation = location;
+                            Log.d(TAG, "Latitude: " + mLastKnownLocation.getLatitude());
+                            Log.d(TAG, "Longitude: " + mLastKnownLocation.getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), Default_zoom));
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            LatLng mDefaultLocation = null;
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(mDefaultLocation, Default_zoom));
+                        }
+
+                    }
+                });
+            }
+        } catch (Exception e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+    private void moveFocus(LatLng latLng,float zoom,String title){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        if (!title.equals("My Location")) {
+
+            MarkerOptions options = new MarkerOptions()
+                    .position(latLng)
+                    .title(title);
+            mMap.addMarker(options);
+        }
+
+    }
+    private void getPermission(){
+        String[] permissions={Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermisssionGranted=true;
+            }
+
+        }else{
+            ActivityCompat.requestPermissions(this,permissions,REQUEST_CODE);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mLocationPermisssionGranted = false;
+        switch (requestCode) {
+            case REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermisssionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermisssionGranted = true;
+                }
+            }
+        }
+    }
+     private void HideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN );
+     }
+}
